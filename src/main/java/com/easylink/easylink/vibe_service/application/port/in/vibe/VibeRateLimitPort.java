c1d4d028e32c7f54ba6fa1659f1cc346f493cf03
Andrey <andrew.saltul@gmail.com>
2025-10-11 21:00:20 -0400

    Creating Vibes error

--- src/main/java/com/easylink/easylink/vibe_service/application/port/in/vibe/VibeRateLimitPort.java
+++ src/main/java/com/easylink/easylink/vibe_service/application/port/in/vibe/VibeRateLimitPort.java
@@ -1,6 +1,6 @@
 package com.easylink.easylink.vibe_service.application.port.in.vibe;
 
 public interface VibeRateLimitPort {
-    boolean canCreateVibe(String userId);
+    boolean canCreateVibe(String userId,long existingVibeCountFromDb);
     void decrementVibe(String userId);
 }
